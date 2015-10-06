4ffe511c21c3a8f0a6fb3b614e6fba0bc64da45d
0uverture <kingmikkel@hotmail.com>
2015-10-06 13:08:09 +0200

    Run modified

--- Assignment05/MicroC/run.bat
+++ Assignment05/MicroC/run.bat
@@ -1,1 +1,1 @@
-fsi -r bin/FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs Interp.fs ParseAndRun.fs
+fsi -r bin/FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs Interp.fs ParseAndRun.fs Machine.fs Comp.fs ParseAndComp.fs Contcomp.fs ParseAndContcomp.fs
