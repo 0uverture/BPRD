ffbf857d2070c8e15aa283a3c3c124783355311d
0uverture <kingmikkel@hotmail.com>
2015-11-05 15:32:07 +0100

    Slowfib.jpeg

---
+++ Assignment08/virtual/slowfibo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/// <summary>
+/// Fibonucci pasta
+/// </summary>
+public class Fibonacci
+{
+    public static int getFibo(int n)
+    {
+        int a = 0;
+        int b = 1;
+        // In N steps compute Fibonacci sequence iteratively.
+        for (int i = 0; i < n; i++)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+        return a;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Lel");
+        Console.In.Read();
+
+        for (int i = 0; i < 100000; i++)
+        {
+            getFibo(100000);    
+        }
+        
+     
+    }
+}
